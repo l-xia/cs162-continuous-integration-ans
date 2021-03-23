068d6ee4da6dbc6c9f1a8bdbc0e7c924d1856f3a
Philip Sterne <sterne@gmail.com>
2021-03-23 10:18:58 +0200

    Migrating off Travis and on to Github actions

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,3 @@
-#because my own tests was not working and writing tests is not central to this
-#assignment, the tests here are made up
-
 import unittest
 
 
@@ -9,17 +6,6 @@ class TestCase(unittest.TestCase):
     def test_connection(self):
         #response = requests.get('http://localhost:5000')
         self.assertEqual(200, 200) #test doesn't work but we are checking continuous integration #response.status_code)
-
-    def test_post(self):
-        #response = requests.post('http://localhost:5000/add', data={'expression':'1+1'})
-        self.assertEqual(200, 200)#test doesn't work but we are checking continuous integration response.status_code)
-
-    def test_err(self):
-        #response = requests.post('http://localhost:5000/add', data={'expressions':'1+1'})
-        self.assertEqual(400, 400)#test doesn't work but we are checking continuous integration response.status_code)
-
-    def test_db(self):
-        self.assertEqual(400, 400)#test doesn't work but we are checking continuous integration response.status_code)
 
 if __name__ == '__main__':
     unittest.main()
